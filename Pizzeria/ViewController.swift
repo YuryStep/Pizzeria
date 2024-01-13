2ba8a2706548a61618eef04a66174394a7d5690a
Yury Stepanchuk <stich-style@mail.ru>
2024-01-13 12:25:49 +0300

    Initial Commit

---
+++ Pizzeria/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pizzeria
+//
+//  Created by Юрий Степанчук on 13.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
